de300a4cf199b5bdfabaed85f2319ab8e3e0a1db
rsstdd <rosstod@gmail.com>
2017-02-17 09:54:12 -0800

    Grabbed category list

--- server.js
+++ server.js
@@ -5,47 +5,55 @@ const cheerio = require('cheerio');
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.get('/scrape', (req, res) => {
-  const url = 'http://www.imdb.com/title/tt1229340/';
+app.get('/list', (req, res) => {
+  // const url ='https://www.wikiwand.com/en/List_of_aircraft_of_World_War_II'
+  const url = 'https://en.wikipedia.org/wiki/List_of_aircraft_of_World_War_II?oldformat=true';
+  const catArr = [];
 
   request(url, (err, res, body) => {
     if (err) {
-      console.log(err);
-    }
-    else if (!err && res.statusCode === 200) {
+      console.err(err);
+    } else if (!err && res.statusCode === 200) {
       const $ = cheerio.load(body);
-      let title, release, rating;
+      let category, name, year_in_service, country_of_origin, operators, link;
 
-      const json = { title: '', release: '', rating: '' };
+      const json = { category: '', name: '', yearInService: '', countryOfOrigin: '', operators: '', link: '' };
 
-      $('.title_wrapper').filter(function() {
+      $('.mw-headline').filter(function() {
         const data = $(this);
 
-        title = data.children().first().text().trim();
-        release = data.children().last().children().text();
+        category = data.text();
 
-        const year = release.trim().split(' ')[47];
-        const month = release.trim().split(' ')[46];
+        catArr.push(category);
 
-        release = `${month} ${year}`;
-
-        json.title = title;
-        json.release = release;
+        json.category = catArr;
       });
 
-      $('.ratingValue').filter(function() {
+      $('.wikitable').filter(function() {
         const data = $(this);
 
-        rating = data.text().split(' ')[0].split('').slice(1, 10).join('');
-        json.rating = rating;
+        let temp = data.text();
+        temp.split(' ')
+        console.log(temp);
+        json.name = rating;
+        json.yearInService = yearInService;
+        json.countryOfOrigin = countryOfOrigin;
+        json.operators = operators;
+        jsonlink = link;
       });
 
       fs.writeFile('output.json', JSON.stringify(json, null, 4), (err) => {
-        console.log('Scrape successful - Check your project directory for the output.json file');
+        if (err) {
+          console.error(err);
+        } else {
+          console.log('Scrape successful - Data Located in the Output.json file');
+        }
       });
     }
   });
 });
+
+// app.get('/aircraft')
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);

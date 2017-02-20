8be4b517a8a3c1697c7c710cb4dfed3daf66ab0d
rsstdd <rosstod@gmail.com>
2017-02-19 16:48:58 -0800

    REformatted URLS to values on obj

---
+++ format.js
@@ -0,0 +1,23 @@
+// const output = require('./output.json');
+//
+// // let categories = output.category.map((item) => console.log(item));
+//
+// // output.name.map((i) => console.log(i));
+//
+// let names = output.name.toString().split(',');
+// fnames = names.filter((item) => item !== 'Name of aircraft');
+//
+// console.log('fnames', fnames.length);
+// let yearInService = output.yearInService.toString().split(',');
+//
+// fYearInSvc = output.yearInService.filter((item) => item !== 'Year in service');
+// console.log('yearInService', fYearInSvc.length);
+//
+//
+// fCountry = output.countryOfOrigin.filter((item) => item !== 'Country of origin');
+// console.log('fCountry', fCountry.length);
+//
+// fOp = output.operators.filter((item) => item !== 'Operator(s) if different from country of origin');
+// console.log('fOp', fOp.length);
+//
+// module.exports = 'format';
--- server.js
+++ server.js
@@ -2,77 +2,99 @@ const express = require('express');
 const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
+const output = require('./output.json');
+const { urls1 } = require('./apages');
 const app = express();
 const port = process.env.PORT || 8000;
 
+const urlstr = 'https://en.wikipedia.org/wiki/'
+
+// console.log(urls1);
+
 app.get('/list', (req, res) => {
-  const url = 'https://en.wikipedia.org/wiki/List_of_aircraft_of_World_War_II?oldformat=true';
-  const catArr = [];
-  const links = [];
+  for (let i = 0; i < urls1.length; i++) {
+    request(`${urlstr}${urls1[i]}`, (err, res, html) => {
+      if (err) {
+        console.log(err);
+      }
+      else if (!err && res.statusCode === 200) {
+        const $ = cheerio.load(html);
+        const aircraft = [];
+        const json = { aircraft: [] };
 
-  request(url, (err, res, html) => {
-    if (err) {
-      console.err(err);
-    } else if (!err && res.statusCode === 200) {
-      const $ = cheerio.load(html);
-      let category,
-          name,
-          yearInService,
-          countryOfOrigin,
-          operators,
-          link;
-      const json = { category: '', name: '', yearInService: '', countryOfOrigin: '', operators: '', link: '' };
+        $('.infobox tr td').each(function() {
+          const data = $(this);
 
-      $('.mw-headline').filter(function() {
-        const data = $(this);
+          console.log(data.text());
 
-        category = data.text();
+          json.aircraft.push(data.text());
+        });
 
-        console.log(category);
-        
-        json.category = category;
-      });
+        $(' .image').each(function() {
+          const data = $(this);
+          const imgUrl = data.attr('href');
 
-      $('.wikitable tr').filter(function() {
-        const data = $(this);
+          aircraft.push(imgUrl);
+        });
 
-        let temp = data.children().text();
-            name = data.children().first().text()
-            yearInService = data.children().first().next().text();
-            operators = data.children().first().next().next().text();
+        const description = $('.body-content').find('p').text();
 
-        // console.log('=====', name, yearInService, operators, '=====');
+        // console.log(description);
 
-        json.name = name;
-        json.yearInService = yearInService;
-        json.countryOfOrigin = countryOfOrigin;
-        json.operators = operators;
-        json.link = link;
-      });
+        aircraft.push(description);
 
-      $('.wikitable a').each(function() {
-        const data = $(this);
+        // console.log(aircraft);
 
-        link = data.attr('href');
-
-        json.link = link;
-      })
-
-      fs.writeFile('output.json', JSON.stringify(json, null, 4), (err) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log('Scrape successful - Data Located in the Output.json file');
-        }
-      });
-    }
-  });
+        // fs.writeFile('output.json', JSON.stringify(json, null, 4), (err) => {
+        //   if (err) {
+        //     console.log(err);
+        //   } else {
+        //     console.log('Scrape successful - Data Located in the Output.json file');
+        //   }
+        // });
+      }
+    });
+  }
 });
-
-// app.get('/aircraft')
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
 module.exports = app;
+
+//
+// $('.mw-headline').filter(function() {
+//   const data = $(this);
+//
+//   category = data.text();
+//
+//   console.log(category);
+//
+//   json.category = category;
+// });
+//
+// $('.wikitable tr').filter(function() {
+//   const data = $(this);
+//
+//   let temp = data.children().text();
+//       name = data.children().first().text()
+//       yearInService = data.children().first().next().text();
+//       operators = data.children().first().next().next().text();
+//
+//   // console.log('=====', name, yearInService, operators, '=====');
+//
+//   json.name = name;
+//   json.yearInService = yearInService;
+//   json.countryOfOrigin = countryOfOrigin;
+//   json.operators = operators;
+//   json.link = link;
+// });
+//
+// $('.wikitable a').each(function() {
+//   const data = $(this);
+//
+//   link = data.attr('href');
+//
+//   json.link = link;
+// })
